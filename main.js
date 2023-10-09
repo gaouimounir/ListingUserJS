8e87decf62347ca656ea41cf5488bff57e2e6024
gaouimounir <mounir.gaoui@outlook.com>
2023-10-09 16:50:53 +0200

    createElement

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 let root = document.getElementById("root");
 
-fetch("https://reqres.in/api/users?page=2")
+fetch("https://reqres.in/api/users?")
   .then((response) => response.json())
   .then((data) => {
     // Utilisez les données récupérées de l'API
     console.log(data);
 
     // Affiche dans le HTML, la temperature récupéré sur l'API
+    const card = document.createElement("div");
+    card.classList.add("card");
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("cardBody");
+    const cardTitle = document.createElement("p");
+    cardTitle.classList.add("cardTitle");
+    const cardImg = document.createElement("img");
+    cardImg.classList.add("cardImg");
+
     let name = document.createElement("h2");
-    name.innerHTML = data.data[0].email;
+    name.innerHTML = data.data[0].first_name + " " + data.data[0].last_name;
+
     root.appendChild(name);
   })
   .catch((error) => {
