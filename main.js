d99a7054eb319807abaf6ab064af81f2db1e0625
gaouimounir <mounir.gaoui@outlook.com>
2023-10-10 10:36:34 +0200

    affichage des cartes avec style

--- main.js
+++ main.js
@@ -6,25 +6,26 @@ fetch("https://reqres.in/api/users?")
     // Utilisez les données récupérées de l'API
     console.log(data);
 
-    // Affiche dans le HTML, la temperature récupéré sur l'API
-    const card = document.createElement("div");
-    card.classList.add("card");
-    const cardBody = document.createElement("div");
-    cardBody.classList.add("cardBody");
-    const cardTitle = document.createElement("h5");
-    cardTitle.classList.add("cardTitle");
-    const cardImg = document.createElement("img");
-    cardImg.src = "https://reqres.in/img/faces/1-image.jpg";
-    cardImg.classList.add("cardImg");
+    // Affiche dans le HTML, la données récupéré sur l'API
+    for (let i = 0; i < data.data.length; i++) {
+      const user = data.data[i];
 
-    cardTitle.innerHTML =
-      data.data[0].first_name + " " + data.data[0].last_name;
+      const card = document.createElement("div");
+      card.classList.add("card");
+      const cardBody = document.createElement("div");
+      cardBody.classList.add("cardBody");
+      const cardTitle = document.createElement("h5");
+      cardTitle.classList.add("cardTitle");
+      const cardImg = document.createElement("img");
+      cardImg.classList.add("cardImg");
 
-    cardImg.innerHTML = data.data[0].avatar;
+      cardImg.src = user.avatar;
+      cardTitle.innerHTML = user.first_name + " " + user.last_name;
 
-    root.appendChild(card);
-    card.appendChild(cardImg);
-    card.appendChild(cardTitle);
+      root.appendChild(card);
+      card.appendChild(cardImg);
+      card.appendChild(cardTitle);
+    }
   })
   .catch((error) => {
     // Gérez les erreurs ici
